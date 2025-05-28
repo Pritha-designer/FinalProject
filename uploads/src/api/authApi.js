9e50ee0b448ec1df3e94265e098061e5f3a91bcb
Pritha-designer <pritha451@gmail.com>
2025-05-28 22:58:12 +0530

    Delete uploads/src directory

--- uploads/src/api/authApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/users' });
-
-export const signup = (userData) => API.post('/signup', userData);
-export const login = (credentials) => API.post('/login', credentials);
